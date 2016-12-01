8a4f4e9ab0d3714ad983a03379ffc96f13df5711
Neven <nevenbucevic@gmail.com>
2016-12-01 17:37:28 +0100

    Added a second View with big emoji

--- Emoji table/ViewController.swift
+++ Emoji table/ViewController.swift
@@ -8,9 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    @IBOutlet var proba: UILabel!
+    var emojis = ["☺️", "😇", "😎", "🤓", "😜", "🐹"]
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
